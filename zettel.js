4bb0210340b555cfa84c2fd3d25efacb12715901
Steven Loria <sloria1@gmail.com>
2016-10-26 00:04:16 -0400

    Add theming

--- zettel.js
+++ zettel.js
@@ -1,12 +1,29 @@
 /* global markdown */
 (function app() {
+  const $ = document.getElementById.bind(document);
+
   const URL = 'http://127.0.0.1:4444/';
   fetch(URL)
     .then(resp => resp.json())
     .then((resp) => {
-      const content = `# ${resp.title} \n`;
+      const content = `# ${resp.title} \n ${resp.content}`;
       const quote = markdown.toHTML(content);
-      const elm = document.getElementById('app');
+      const elm = $('app');
       elm.innerHTML = quote;
     });
+
+
+  const lightTheme = 'css/zettel.css';
+  const darkTheme = 'css/zettel-dark.css';
+  const linkElement = $('styleLink');
+  const themeButton = $('themeButton');
+  function themeSwitch() {
+    const newStyle = (linkElement.getAttribute('href') === lightTheme) ? darkTheme : lightTheme;
+    linkElement.setAttribute('href', newStyle);
+    localStorage.theme = newStyle;
+  }
+  let currentStyle = localStorage.theme;
+  if (currentStyle == undefined) currentStyle = lightTheme;  // eslint-disable-line
+  linkElement.setAttribute('href', currentStyle);
+  themeButton.addEventListener('click', themeSwitch);
 }());
