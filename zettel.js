6b51e44b3b55edff3dd08a34be4b4c2183d85f9a
Steven Loria <sloria1@gmail.com>
2016-10-25 23:19:17 -0400

    Linting

--- zettel.js
+++ zettel.js
@@ -1,12 +1,12 @@
-/* jshint esnext: true, unused: true */
-(function(){
+/* global markdown */
+(function app() {
   const URL = 'http://127.0.0.1:4444/';
   fetch(URL)
-    .then((resp) => resp.json())
+    .then(resp => resp.json())
     .then((resp) => {
-      let content = '# ' + resp.title + '\n' + resp.content;
-      let quote = markdown.toHTML(content);
-      var elm = document.getElementById('app');
+      const content = `# ${resp.title} \n`;
+      const quote = markdown.toHTML(content);
+      const elm = document.getElementById('app');
       elm.innerHTML = quote;
     });
-})();
+}());
